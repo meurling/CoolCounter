673873ea73aeae6dfc5ca4577918a2e7c8c0ed8e
Johan Vikström <tritzium@gmail.com>
2017-11-11 16:30:23 +0100

    Added REACT-NAVIGATION!

--- Meet/App.js
+++ Meet/App.js
@@ -2,15 +2,56 @@ import React, { Component } from 'react';
 import allReducers from './src/reducers/index.js';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
+import {connect} from 'react-redux';
+
+import { addNavigationHelpers } from 'react-navigation';
+import {AppNavigator} from './src/reducers/navReducer.js';
 import Counter from './src/components/counter.js';
 
 const store = createStore(allReducers);
-export default class App extends Component{
+class App extends Component{
   render(){
     return(
-      <Provider store= {store}>
-        <Counter />
-      </Provider>
+      <AppNavigator navigation={addNavigationHelpers({
+        dispatch: this.props.dispatch,
+        state: this.props.nav
+      })} />
     );
   }
 }
+
+const mapStateToProps = (state) => ({
+  nav: state.nav
+});
+
+const AppWithNavigationState = connect(mapStateToProps)(App);
+
+
+export default class Root extends Component{
+  render(){
+    return(
+      <Provider store={store}>
+        <AppWithNavigationState/>  
+      </Provider>
+    )
+  }
+}
+
+/*
+
+class App extends React.Component {
+  render() {
+    return (
+      <AppNavigator navigation={addNavigationHelpers({
+        dispatch: this.props.dispatch,
+        state: this.props.nav,
+      })} />
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  nav: state.nav
+});
+
+*/
---
+++ Meet/src/routes.js
@@ -0,0 +1,11 @@
+import Counter from './components/counter.js';
+import NextScreen from './components/nextScreen.js';
+
+export const AppRouteConfigs = {
+        Counter: {
+            screen: Counter
+        },
+        NextScreen: {
+            screen: NextScreen
+        }
+}
